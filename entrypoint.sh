d8f878b7662fa124b703dda6461c3d52558ddf9f
Niall Buckley <buckleyniall100@gmail.com>
2025-04-08 16:47:33 +0200

    remove conditional

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,30 +25,15 @@ echo "Git repo: $git_refs_url"
 
 echo "**pushing tag $tag to repo $GITHUB_REPOSITORY"
 
-if $tag_exists
-then
-  # update tag
-  curl -s -X PATCH "$git_refs_url/tags/$TAG" \
-  -H "Authorization: token $GITHUB_TOKEN" \
-  -d @- << EOF
-
-  {
-    "sha": "$GITHUB_SHA",
-    "force": true
-  }
-EOF
-else
-  # create new tag
-  reponse=$(curl -s -X POST $git_refs_url -H "Authorization: token $GITHUB_TOKEN" -d @- << EOF
+# create new tag
+reponse=$(curl -s -X POST $git_refs_url -H "Authorization: token $GITHUB_TOKEN" -d @- << EOF
 { "ref": "refs/tags/$TAG", "sha": "$GITHUB_SHA" }
 EOF
 )
+echo "My response is $response"
 status=$(echo "$response" | jq -r '.status // empty')
 echo "Status code is $status"
-fi
 
-status=$(echo "$response" | jq -r '.status // empty')
-echo "Status code is $status"
 if [ "$status" == "422" ]; then
 	echo "Tag already exists. Skipping creation."
 	exit 1
